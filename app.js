9c047a97058cc136bb239d213e911b132de4bcfc
Alpay Özer <alpayozer7@gmail.com>
2024-05-14 11:35:33 +0300

    calculator finished

--- app.js
+++ app.js
@@ -2,39 +2,39 @@ let numberScreen = document.getElementById("numberScreen");
 let equalButton = document.getElementById("equalButton");
 let allClearButton = document.getElementById("allClearButton");
 let clearButton = document.getElementById("clearButton");
-let buffer = "";
-let firstBuffer = 0;
-let operator = "";
-let isSubtraction = false;
 
-const numbers = (value) => {
-  numberScreen.value += value;
-  buffer += value;
-};
+let operators = ["+", "-", "*", "/", "%", "="];
+let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
+let isOperator;
+let isNumber;
+let buffer = "";
+let operator = "";
+let firstBuffer = 0;
+let result = 0;
 
 const getMode = (number1, number2) => {
-  numberScreen.value = parseFloat(number1) % parseFloat(number2);
-  return numberScreen.value;
+  result = parseFloat(number1) % parseFloat(number2);
+  return result;
 };
 
 const sum = (number1, number2) => {
-  numberScreen.value = parseFloat(number1) + parseFloat(number2);
-  return numberScreen.value;
+  result = parseFloat(number1) + parseFloat(number2);
+  return result;
 };
 
 const substraction = (number1, number2) => {
-  numberScreen.value = parseFloat(number1) - parseFloat(number2);
-  return numberScreen.value;
+  result = parseFloat(number1) - parseFloat(number2);
+  return result;
 };
 
 const multiplication = (number1, number2) => {
-  numberScreen.value = (parseFloat(number1) * parseFloat(number2)).toFixed(2);
-  return numberScreen.value;
+  result = (parseFloat(number1) * parseFloat(number2)).toFixed(2);
+  return result;
 };
 
 const division = (number1, number2) => {
-  numberScreen.value = (parseFloat(number1) / parseFloat(number2)).toFixed(2);
-  return numberScreen.value;
+  result = (parseFloat(number1) / parseFloat(number2)).toFixed(2);
+  return result;
 };
 
 const clearScreen = () => {
@@ -49,66 +49,76 @@ const clearOne = () => {
   buffer = buffer.slice(0, -1);
 };
 
-const operators = (value) => {
-  switch (value) {
+const calculator = (value) => {
+  console.log(value);
+  numberScreen.value += value;
+  isOperator = operators.includes(value);
+  isNumber = numbers.includes(value);
+
+  if (isNumber) {
+    buffer += value;
+  }
+
+  if (isOperator) {
+    if (operator !== "") {
+      calculate(operator);
+    } else {
+      firstBuffer = buffer;
+      buffer = "";
+    }
+
+    operator = value;
+
+    if (operator == "=") {
+      numberScreen.value = firstBuffer;
+      buffer = firstBuffer;
+      firstBuffer = 0;
+      operator = "";
+    }
+  }
+};
+
+const calculate = (operator) => {
+  switch (operator) {
     case "+":
       {
-        numberScreen.value += value;
-        operator = value;
-        firstBuffer += parseFloat(buffer);
+        result = sum(firstBuffer, buffer);
+        firstBuffer = result;
         buffer = "";
+        operator = "";
       }
       break;
     case "-":
       {
-        numberScreen.value += value;
-        operator = value;
-        firstBuffer = parseFloat(buffer);
+        result = substraction(firstBuffer, buffer);
+        firstBuffer = result;
         buffer = "";
+        operator = "";
       }
       break;
     case "*":
       {
-        numberScreen.value += value;
-        operator = value;
-        firstBuffer = parseFloat(buffer);
+        result = multiplication(firstBuffer, buffer);
+        firstBuffer = result;
         buffer = "";
+        operator = "";
       }
       break;
     case "/":
       {
-        numberScreen.value += value;
-        operator = value;
-        firstBuffer = parseFloat(buffer);
+        result = division(firstBuffer, buffer);
+        firstBuffer = result;
         buffer = "";
+        operator = "";
       }
       break;
     case "%":
       {
-        numberScreen.value += value;
-        operator = value;
-        firstBuffer = parseFloat(buffer);
+        result = getMode(firstBuffer, buffer);
+        firstBuffer = result;
         buffer = "";
+        operator = "";
       }
       break;
-  }
-};
-
-equalButton.onclick = () => {
-  if (operator == "+") {
-    buffer = sum(firstBuffer, buffer);
-    firstBuffer = 0;
-  } else if (operator == "-") {
-    buffer = substraction(firstBuffer, buffer);
-    firstBuffer = 0;
-  } else if (operator == "/") {
-    buffer = division(firstBuffer, buffer);
-    firstBuffer = 0;
-  } else if (operator == "*") {
-    buffer = multiplication(firstBuffer, buffer);
-    firstBuffer = 0;
-  } else if (operator == "%") {
-    buffer = getMode(firstBuffer, buffer);
-    firstBuffer = 0;
   }
 };
